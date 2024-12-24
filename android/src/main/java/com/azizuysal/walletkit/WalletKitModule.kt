e5e477c17a3f68a1b8dd24c635e352f955cec53d
Aziz Uysal <1683443+azizuysal@users.noreply.github.com>
2024-12-24 01:29:49 -0800

    fix: fixed android build

--- android/src/main/java/com/azizuysal/walletkit/WalletKitModule.kt
+++ android/src/main/java/com/azizuysal/walletkit/WalletKitModule.kt
@@ -2,11 +2,17 @@ package com.azizuysal.walletkit
 
 import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.ReadableArray
+import com.facebook.react.bridge.WritableMap
+import com.facebook.react.modules.core.DeviceEventManagerModule
 
 class WalletKitModule(reactContext: ReactApplicationContext) :
   ReactContextBaseJavaModule(reactContext) {
+
+  private var listenerCount = 0
 
   override fun getName(): String {
     return NAME
@@ -14,21 +20,22 @@ class WalletKitModule(reactContext: ReactApplicationContext) :
 
   companion object {
     const val NAME = "WalletKit"
+    private const val ERR_WALLET_NOT_IMPLEMENTED = "ERR_WALLET_NOT_IMPLEMENTED"
   }
 
   @ReactMethod
   fun canAddPasses(promise: Promise) {
-    promise.reject("Not Implemented")
+    promise.reject(ERR_WALLET_NOT_IMPLEMENTED, "Not Implemented")
   }
 
   @ReactMethod
   fun addPass(passData: String?, promise: Promise) {
-    promise.reject("Not Implemented")
+    promise.reject(ERR_WALLET_NOT_IMPLEMENTED, "Not Implemented")
   }
 
   @ReactMethod
   fun addPasses(passDataArray: ReadableArray?, promise: Promise) {
-    promise.reject("Not Implemented")
+    promise.reject(ERR_WALLET_NOT_IMPLEMENTED, "Not Implemented")
   }
 
   @ReactMethod
@@ -37,14 +44,14 @@ class WalletKitModule(reactContext: ReactApplicationContext) :
   }
 
   @ReactMethod
-  fun removeListeners(count: Integer) {
+  fun removeListeners(count: Int) {
     listenerCount -= count
   }
 
   private fun sendEvent(
     reactContext: ReactContext,
     eventName: String,
-    @Nullable params: WritableMap
+    params: WritableMap?
   ) {
     reactContext
       .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
